47994d2d62c517eb2f80e76beb969adbccd47a84
YoungMin Huh”git config --global user.name YoungMin <pirant26@gmail.com>
2023-03-21 17:21:43 +0900

    블로그 포스트 상세 페이지

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 정수 형태로 들어오는 url이라면 int 변수를 pk에 담아 blog/views의 single_post_page() 함수를 사용하라
+    path('<int:pk>', views.single_post_page),
     path('', views.index),
 ]
